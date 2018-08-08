47474f9e68fabfdc06bd6fad15c3f07204c47f76
levyfulop <levyfulop@yahoo.com>
2018-08-08 09:44:30 +0200

    initial commit

---
+++ GetAllVMs.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from pyVmomi import vim
+from tools import cli
+from pyVim.connect import SmartConnectNoSSL, Disconnect
+import atexit
+import getpass
+import time
+import platform
+import json
+start_time = time.time()
+
+print(platform.python_version())
+
+
+def main():
+
+    args = cli.get_args()
+
+    # Connect to the host without SSL signing
+    try:
+        si = SmartConnectNoSSL(
+            host=args.host,
+            user=args.user,
+            pwd=args.password,
+            port=int(args.port))
+        atexit.register(Disconnect, si)
+
+    except IOError as e:
+        pass
+
+    if not si:
+        raise SystemExit("Unable to connect to host with supplied info.")
+
+    content = si.RetrieveContent()
+
+    # create a list of vim.VirtualMachine objects so
+    # that we can query them for statistics
+    container = content.rootFolder
+    viewType = [vim.VirtualMachine]
+    recursive = True
+
+    containerView = content.viewManager.CreateContainerView(container,
+                                                            viewType,
+                                                            recursive)
+    children = containerView.view
+    for child in children:
+     print(child)
+
+
+if __name__ == "__main__":
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
